894bba09759a87dd52f2721d973e96253c021128
Himanshu Khowal <khowalhimanshu1@gmail.com>
2022-09-09 06:04:40 +0530

    test

--- live.php
+++ live.php
@@ -17,8 +17,10 @@ if ($token != "" && @$_REQUEST["c"] != "") {
     error_log(print_r($opts, TRUE)); 
     $cx = stream_context_create($opts);
     $hs = file_get_contents("https://jiotv.live.cdn.jio.com/" . $_REQUEST["c"] . "/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] . ".m3u8" . $token, false, $cx);
+    error_log(print_r($hs, TRUE)); 
     $hs = @preg_replace("/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] . "-([^.]+\.)key/", 'stream.php?key=' . $_REQUEST["c"] . '/' . $_REQUEST["c"] . '_' . $_REQUEST["q"] . '-\1key', $hs);
     $hs = @preg_replace("/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] . "-([^.]+\.)ts/", 'stream.php?ts=' . $_REQUEST["c"] . '/' . $_REQUEST["c"] . '_' . $_REQUEST["q"] . '-\1ts', $hs);
     $hs = str_replace("https://tv.media.jio.com/streams_live/" . $_REQUEST["c"] . "/", "", $hs);
+    error_log(print_r($hs, TRUE)); 
     print $hs;
 }
